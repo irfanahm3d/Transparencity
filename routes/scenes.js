c1fed9e1a8617277f06655a09e7ef2ed0e1ecf4f
Irfan Ahmed <irfanahm3d@live.com>
2018-11-30 16:08:52 +0500

    Included new api routes. Scenes: GET scenes, GET scenes by userId, GET scenes by issueCategory, PUT scene using _id. Users: GET user by userId, GET user by email, PUT user using userId

--- routes/scenes.js
+++ routes/scenes.js
@@ -3,21 +3,55 @@ var express = require('express');
 var router = express.Router();
 var SceneModel = require('../models/scene.model');
 
-/* GET scene by id. */
-router.get('/scene', function (req, res) {
+/*
+ * GET all scenes.
+ * Limit to 10
+ * TODO: paging of data
+ */
+router.get('', function (req, res) {
+    SceneModel.find()
+        .limit(10)
+        .then(docs => { res.json(docs); })
+        .catch(err => { res.status(500).json(err); });
+});
+
+/*
+ * GET scene by id.
+ */
+router.get('/scene/id', function (req, res) {
     if (!req.body.id) {
         return res.status(400).send('Missing URL parameter: id');
     }
 
-    SceneModel.findOne({
-        _id: req.body.id
-    })
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+    SceneModel.findOne({ _id: req.body.id })
+        .then(doc => { res.json(doc); })
+        .catch(err => { res.status(500).json(err); });
+});
+
+/*
+ * GET scenes by userId.
+ */
+router.get('/userId', function (req, res) {
+    if (!req.body.userId) {
+        return res.status(400).send('Missing URL parameter: userId');
+    }
+
+    SceneModel.find({ userId: req.body.userId })
+        .then(docs => { res.json(docs); })
+        .catch(err => { res.status(500).json(err); });
+});
+
+/*
+ * GET scenes by issueCategory.
+ */
+router.get('/issueCategory', function (req, res) {
+    if (!req.body.issueCategory) {
+        return res.status(400).send('Missing URL parameter: issueCategory');
+    }
+
+    SceneModel.find({ issueCategory: req.body.issueCategory })
+        .then(docs => { res.json(docs); })
+        .catch(err => { res.status(500).json(err); });
 });
 
 /* 
@@ -41,6 +75,35 @@ router.post('/scene', function (req, res) {
             }
 
             res.status(201).send(doc);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+/*
+ * Update a scene
+ * PUT scene
+ */
+router.put('/scene', function (req, res) {
+    if (!req.body) {
+        return res.status(400).send('Request body is missing');
+    }
+
+    if (!req.body.sceneId) {
+        return res.status(400).send('Missing parameter: sceneId');
+    }
+
+    SceneModel.findOneAndUpdate(
+        { _id: req.body.sceneId },
+        req.body,
+        { new: true },
+        function (err, doc) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            res.status(200).send(doc);
         })
         .catch(err => {
             res.status(500).json(err);
--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,38 @@ var express = require('express');
 var router = express.Router();
 var UserModel = require('../models/user.model');
 
-/* GET user by name. */
-router.get('/user', function (req, res) {
+/* 
+ * Retrieve a user
+ * GET user by userId
+ */
+router.get('/user/id', function (req, res) {
     if (!req.body.userId) {
         return res.status(400).send('Missing URL parameter: userId');
     }
 
-    UserModel.findOne({
-            userId: req.body.userId
-        })
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+    UserModel.findOne({ userId: req.body.userId })
+        .then(doc => { res.json(doc); })
+        .catch(err => { res.status(500).json(err); });
+});
+
+/* 
+ * Retrieve a user
+ * GET user by email
+ */
+router.get('/user/email', function (req, res) {
+    if (!req.body.email) {
+        return res.status(400).send('Missing URL parameter: email');
+    }
+
+    UserModel.findOne({ email: req.body.email })
+        .then(doc => { res.json(doc); })
+        .catch(err => { res.status(500).json(err); });
 });
 
 /* 
  * Create a new user
  * POST user 
-*/
+ */
 router.post('/user', function (req, res) {
     if (!req.body) {
         return res.status(400).send('Request body is missing');
@@ -41,6 +52,35 @@ router.post('/user', function (req, res) {
             }
 
             res.status(201).send(doc);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+/*
+ * Update a user
+ * PUT user
+ */
+router.put('/user', function (req, res) {
+    if (!req.body) {
+        return res.status(400).send('Request body is missing');
+    }
+
+    if (!req.body.userId) {
+        return res.status(400).send('Missing parameter: userId');
+    }
+
+    UserModel.findOneAndUpdate(
+        { userId: req.body.userId },
+        req.body,
+        { new: true },
+        function (err, doc) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            res.status(200).send(doc);
         })
         .catch(err => {
             res.status(500).json(err);
